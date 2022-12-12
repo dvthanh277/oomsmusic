d200b7e45e54e929a3f8aae7bea49b6955a663ac
Do Thanh <dvthanh277@aiaracorp.com>
2022-12-12 09:12:32 +0700

    Update

--- src/ultis/axios.js
+++ src/ultis/axios.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const request = axios.create({
-    // baseURL: `https://zingmp3api.vercel.app/ooms-api`,
-    baseURL: `http://localhost:9001/ooms-api`,
+    baseURL: `https://zingmp3api.vercel.app/ooms-api`,
+    // baseURL: `http://localhost:9001/ooms-api`,
 });
 
 request.interceptors.response.use(
